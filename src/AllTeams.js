5eccead4dd5420f3b2de73e126cc4d8d50fd3dfb
enriqueamate <enriqueaamate@gmail.com>
2022-12-05 13:58:44 -0600

    Stylize AllTeams component, Add useFakeData func

--- src/AllTeams.js
+++ src/AllTeams.js
@@ -2,10 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import fakeData from "./teamData.json";
 import "./App.css";
 import { getApiKey } from "./utils";
+import { getFakeData } from "./utils";
+import Alert from "@mui/material/Alert";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import StandingsTable from "./StandingsTable";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const teamQuery = () => {
   const apiKey = getApiKey();
+  const useFakeData = getFakeData();
 
   const options = {
     method: "GET",
@@ -14,13 +21,16 @@ const teamQuery = () => {
       "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
     },
   };
-
-  return fetch(
-    `https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022`,
-    options
-  )
-    .then((response) => response.json())
-    .catch((err) => console.error(err));
+  if (useFakeData === true) {
+    return fakeData;
+  } else {
+    return fetch(
+      `https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022`,
+      options
+    )
+      .then((response) => response.json())
+      .catch((err) => console.error(err));
+  }
 };
 
 function AllTeams() {
@@ -33,14 +43,79 @@ function AllTeams() {
   //   const isLoading = false;
   //   const error = false;
 
-  if (isLoading) return "Loading...";
-  if (error) return "An error has occurred: " + error.message;
+  const loadingElement = (
+    <Backdrop
+      sx={{ color: "#000", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+
+  if (isLoading) return loadingElement;
+  if (error)
+    return (
+      <Alert severity="error">
+        This is an error alert — check it out! {error.message}
+      </Alert>
+    );
 
   return (
-    <div>
-      <StandingsTable data={data.response} conference="east" />
-      <StandingsTable data={data.response} conference="west" />
-    </div>
+    <Stack
+      direction="row"
+      spacing={10}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack direction="column" alignItems="center">
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <Typography
+            sx={{
+              color: "red",
+              fontStyle: "bold",
+              fontWeight: "900",
+              fontSize: "2em",
+            }}
+          >
+            EASTERN
+          </Typography>
+          <Typography
+            sx={{
+              color: "black",
+              fontWeight: "100",
+              fontSize: "2em",
+            }}
+          >
+            CONFERENCE
+          </Typography>
+        </Stack>
+        <StandingsTable data={data.response} conference="east" />
+      </Stack>
+
+      <Stack direction="column" alignItems="center">
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <Typography
+            sx={{
+              color: "red",
+              fontStyle: "bold",
+              fontWeight: "900",
+              fontSize: "2em",
+            }}
+          >
+            WESTERN
+          </Typography>
+          <Typography
+            sx={{
+              color: "black",
+              fontWeight: "100",
+              fontSize: "2em",
+            }}
+          >
+            CONFERENCE
+          </Typography>
+        </Stack>
+        <StandingsTable data={data.response} conference="west" />
+      </Stack>
+    </Stack>
   );
 }
 
