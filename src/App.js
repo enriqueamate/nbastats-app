d436d29723ad8793a1c79dba3a63448de7d95a5d
enriqueamate <enriqueaamate@gmail.com>
2022-12-05 01:01:00 -0600

    Display Data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
+import AllGames from "./AllGames";
+import AllTeams from "./AllTeams";
+import { getApiKey } from "./utils";
+
+const queryClient = new QueryClient();
 
 function App() {
+  const apiKey = getApiKey();
+  if (!apiKey) {
+    const message = "Please provide an apiKey param in the URL";
+    alert(message);
+    return <h1>{message}</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          NBA STATS LIVE
-          
-        </p>
-        
-
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <AllGames />
+      <AllTeams />
+    </QueryClientProvider>
   );
 }
 
