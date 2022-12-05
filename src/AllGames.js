8d74690abaf49d2aab99b0b8284e378760399c5c
enriqueamate <enriqueaamate@gmail.com>
2022-12-05 13:59:03 -0600

    Stylize AllGames component, Add useFakeData func

--- src/AllGames.js
+++ src/AllGames.js
@@ -2,12 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import fakeData from "./data.json";
 import "./App.css";
 import { getApiKey } from "./utils";
+import { getFakeData } from "./utils";
 import MatchCard from "./MatchCard";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function gameQuery() {
   const apiKey = getApiKey();
+  const useFakeData = getFakeData();
 
   const options = {
     method: "GET",
@@ -22,12 +26,16 @@ function gameQuery() {
   const day = String(today.getDate()).padStart(2, "0");
   const formattedDate = today.getFullYear() + "-" + month + "-" + day;
 
-  return fetch(
-    `https://api-nba-v1.p.rapidapi.com/games?date=${formattedDate}`,
-    options
-  )
-    .then((response) => response.json())
-    .catch((err) => console.error(err));
+  if (useFakeData) {
+    return fakeData;
+  } else {
+    return fetch(
+      `https://api-nba-v1.p.rapidapi.com/games?date=${formattedDate}`,
+      options
+    )
+      .then((response) => response.json())
+      .catch((err) => console.error(err));
+  }
 }
 
 function Games(props) {
@@ -61,17 +69,56 @@ function Games(props) {
   }
 
   if (gamesArr.length === 0 && status === "Live") {
-    return <h1>No se encontraron juegos en vivo </h1>;
+    return (
+      <Stack direction="row" justifyContent="center">
+        <Typography
+          sx={{
+            m: 5,
+            color: "grey",
+            fontStyle: "bold",
+            fontWeight: "100",
+            fontSize: "1em",
+          }}
+        >
+          No live matches were found 🥺
+        </Typography>
+      </Stack>
+    );
   } else if (gamesArr.length === 0 && status === "Scheduled") {
-    return <h1>No se encontraron juegos programados para hoy</h1>;
+    return (
+      <Stack direction="row" justifyContent="center">
+        <Typography
+          sx={{
+            m: 5,
+            color: "grey",
+            fontStyle: "bold",
+            fontWeight: "100",
+            fontSize: "1em",
+          }}
+        >
+          No scheduled matches were found 🥺
+        </Typography>
+      </Stack>
+    );
   } else if (gamesArr.length === 0 && status === "Finished") {
-    return <h1>No se encontraron juegos finalizados hoy</h1>;
+    return (
+      <Stack direction="row" justifyContent="center">
+        <Typography
+          sx={{
+            m: 3,
+            color: "grey",
+            fontStyle: "bold",
+            fontWeight: "100",
+            fontSize: "1em",
+          }}
+        >
+          No finished matches were found for today 🥺
+        </Typography>
+      </Stack>
+    );
   }
-
   return (
     <div>
-      {/* AGREGAR STATUS MESSAGE */}
-      <Typography></Typography>
       <Stack
         direction="row"
         spacing={2}
@@ -86,11 +133,88 @@ function Games(props) {
 
 function AllGames() {
   return (
-    <div>
+    <Stack direction="column" justifyContent="center" alignItems="center">
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{ marginTop: "20px", marginBottom: "20px" }}
+      >
+        <Typography
+          sx={{
+            color: "red",
+            fontStyle: "bold",
+            fontWeight: "900",
+            fontSize: "2em",
+          }}
+        >
+          FINISHED
+        </Typography>
+        <Typography
+          sx={{
+            color: "black",
+            fontWeight: "100",
+            fontSize: "2em",
+          }}
+        >
+          GAMES
+        </Typography>
+      </Stack>
       <Games status="Finished" />
+
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{ marginTop: "100px", marginBottom: "20px" }}
+      >
+        <Typography
+          sx={{
+            color: "red",
+            fontStyle: "bold",
+            fontWeight: "900",
+            fontSize: "2em",
+          }}
+        >
+          SCHEDULED
+        </Typography>
+        <Typography
+          sx={{
+            color: "black",
+            fontWeight: "100",
+            fontSize: "2em",
+          }}
+        >
+          GAMES
+        </Typography>
+      </Stack>
       <Games status="Scheduled" />
+
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{ marginTop: "100px", marginBottom: "20px" }}
+      >
+        <Typography
+          sx={{
+            color: "red",
+            fontStyle: "bold",
+            fontWeight: "900",
+            fontSize: "2em",
+          }}
+        >
+          LIVE
+        </Typography>
+        <Typography
+          sx={{
+            color: "black",
+            fontWeight: "100",
+            fontSize: "2em",
+          }}
+        >
+          GAMES
+        </Typography>
+      </Stack>
       <Games status="Live" />
-    </div>
+    </Stack>
   );
 }
 
